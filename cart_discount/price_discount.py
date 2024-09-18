199407bedbe97bd13517142322e699fff3d472bc
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-09-18 17:06:39 -0500

    Implement discount function

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -2,15 +2,21 @@ def main():
 
     print(discount([10, 4, 20]))  # Expect this to print 4
     # what other lists might this function be called with?
-    
+
 
 def discount(item_prices):
-    """ Complete this function that returns the discount earned for a list of item prices
-    If a customer has ordered three or more items, the cheapest item is free.
-    Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
+    Accepts an array of item prices and returns the discount earned.
+    Discounts are only earned when there are three or more prices, in which
+    case the discount is the price of the cheapest item.
 
-    pass  # todo replace this line with your code 
+    Args:
+        item_prices (list): List of numerical prices.
+
+    Returns:
+        int: The discount earned.
+    """
+    return sorted(item_prices)[0] if len(item_prices) > 2 else 0
 
 
 if __name__ == '__main__':
